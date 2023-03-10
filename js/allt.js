4b75cc27544d7f07b52df0d20107c3d27bd3f218
AaaicileF <felicia.rosklintwimmer@elev.ga.ntig.se>
2023-03-10 14:33:36 +0100

    Gjort ändringar på 1a, 2a och 3e sidan
    Lagt in bilder och andra ändringar

--- js/allt.js
+++ js/allt.js
@@ -3,14 +3,14 @@ document.querySelector(".fade-layer").addEventListener("click",showMenu);
 
 document.querySelector(".menu-button").addEventListener("click",showMenu); 
 
-
+// document.querySelector("nav.menu").addEventListener("click",); 
 
 function showMenu(){
     let menu = document.querySelector("nav.menu");
     menu.classList.toggle("show");
 
     let layer = document.querySelector(".fade-layer");
-    layer.classList.toggle("visible")   
+    layer.classList.toggle("visible");   
 }
 
 
